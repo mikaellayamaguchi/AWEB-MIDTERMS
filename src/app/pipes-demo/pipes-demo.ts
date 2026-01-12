854b7ab8e89f69bfca9e389cb512cb41033bc8a3
mikaellayamaguchi23 <mikaellayamaguchi23@gmail.com>
2026-01-13 07:34:58 +0800

    m

---
+++ src/app/pipes-demo/pipes-demo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes-demo',
+  imports: [],
+  templateUrl: './pipes-demo.html',
+  styleUrl: './pipes-demo.css',
+})
+export class PipesDemo {
+
+}
