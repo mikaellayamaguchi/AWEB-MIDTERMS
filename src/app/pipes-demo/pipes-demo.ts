c4e2d5eea38dc7540bcdce3bc6a36f3a2a2e6118
mikaellayamaguchi23 <mikaellayamaguchi23@gmail.com>
2026-01-13 08:16:34 +0800

    pipes

--- src/app/pipes-demo/pipes-demo.ts
+++ src/app/pipes-demo/pipes-demo.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { AsyncPipe, CurrencyPipe, DatePipe, DecimalPipe, LowerCasePipe, SlicePipe, UpperCasePipe, PercentPipe, KeyValuePipe, TitleCasePipe } from '@angular/common';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pipes-demo',
-  imports: [],
+  standalone: true,
+  imports: [
+    DatePipe,
+    UpperCasePipe,
+    LowerCasePipe,
+    CurrencyPipe,
+    SlicePipe,
+    DecimalPipe,
+    AsyncPipe,
+    PercentPipe,
+    KeyValuePipe,
+    TitleCasePipe
+  ],
   templateUrl: './pipes-demo.html',
-  styleUrl: './pipes-demo.css',
+  styleUrls: ['./pipes-demo.css'],
 })
 export class PipesDemo {
+  presentDate = new Date();
+  price: number = 20000;
 
+  Fruits = ["Apple", "Orange", "Grapes", "Mango", "Kiwi", "Pomegranate"];
+
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+
+  time$ = interval(1000)
+    .pipe(map(val => new Date()));
+
+  // Example object for KeyValuePipe
+  fruitObj = {
+    Apple: 10,
+    Orange: 20,
+    Mango: 15
+  };
+
+  sampleText = "hello angular";
 }
